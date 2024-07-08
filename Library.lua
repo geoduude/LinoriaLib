f4e5847a5d11b6adf672fe7f627b0d59c1290f37
geoduude <70681785+geoduude@users.noreply.github.com>
2024-07-07 21:04:39 -0400

    fuck no bayybeh

--- Library.lua
+++ Library.lua
@@ -3390,20 +3390,6 @@ function Library:CreateWindow(...)
 		ZIndex = 2;
 		Parent = MainSectionInner;
 	});
-	
-	local InnerVideoBackground = Library:Create('VideoFrame', {
-		BackgroundColor3 = Library.MainColor;
-		BorderMode = Enum.BorderMode.Inset;
-		BorderSizePixel = 0;
-		Position = UDim2.new(0, 1, 0, 1);
-		Size = UDim2.new(1, -2, 1, -2);
-		ZIndex = 2;
-		Visible = false;
-		Volume = 0;
-		Looped = true;
-		Parent = TabContainer;
-	});
-	Library.InnerVideoBackground = InnerVideoBackground;
 
 	Library:AddToRegistry(TabContainer, {
 		BackgroundColor3 = 'MainColor';
