62cf6ad096c99f1b591cbc22164664fd1ccf15b6
geodude <70681785+geoduude@users.noreply.github.com>
2024-10-23 04:22:16 -0400

    ;w; URGH

--- Library.lua
+++ Library.lua
@@ -3768,7 +3768,7 @@ function Library:CreateWindow(...)
 
 				local Container = Library:Create('Frame', {
 					BackgroundTransparency = 1;
-					Position = UDim2.new(0, 4, 0, 42);
+					Position = UDim2.new(0, 4, 0, 43);
 					Size = UDim2.new(1, -4, 1, -20);
 					ZIndex = 1;
 					Visible = false;
