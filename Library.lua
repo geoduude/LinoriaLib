8e843958aee9872346641722f6af44ed54f81a8d
geodude <70681785+geoduude@users.noreply.github.com>
2024-10-23 04:00:10 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3667,7 +3667,7 @@ function Library:CreateWindow(...)
 				BackgroundColor3 = Library.BackgroundColor;
 				BorderColor3 = Library.OutlineColor;
 				BorderMode = Enum.BorderMode.Inset;
-				Size = UDim2.new(1, 0, 0, 20);
+				Size = UDim2.new(1, 0, 0, 0);
 				ZIndex = 2;
 				Parent = Info.Side == 1 and LeftSide or RightSide;
 			});
@@ -3681,7 +3681,7 @@ function Library:CreateWindow(...)
 				BackgroundColor3 = Library.BackgroundColor;
 				BorderColor3 = Color3.new(0, 0, 0);
 				-- BorderMode = Enum.BorderMode.Inset;
-				Size = UDim2.new(1, -2, 1, 18);
+				Size = UDim2.new(1, -2, 1, -2);
 				Position = UDim2.new(0, 1, 0, 1);
 				ZIndex = 4;
 				Parent = BoxOuter;
