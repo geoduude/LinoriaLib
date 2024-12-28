1d7ff02fb3893251c537cb722173b470b13b8134
geodude <70681785+geoduude@users.noreply.github.com>
2024-12-28 12:54:20 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -107,13 +107,13 @@ local function GetPlayersString()
 end;
 
 local function GetOtherPlayersString()
-	local PlayerList = {}
+	local PlayerList = {};
 
 	for i, Player in Players:GetPlayers() do
-		if Player == game.Players.LocalPlayer then continue end
-
-		PlayerList[i] = Player.Name;
-	end
+		if Player ~= game.Players.LocalPlayer then
+			PlayerList[i] = Player.Name;
+		end;
+	end;
 
 	table.sort(PlayerList, function(str1, str2) return str1 < str2 end);
 
